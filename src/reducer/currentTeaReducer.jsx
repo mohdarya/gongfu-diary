3426aa1a26b910905e27477f6c0344ca77d555d7
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-09-05 18:07:36 +0400

    added basic reducer

---
+++ src/reducer/currentTeaReducer.jsx
@@ -0,0 +1,25 @@
+/*
+
+{
+teaName:
+Origin:
+Vendor:
+Link:
+}
+ */
+
+const initialState = {
+
+    teaAvailable: [],
+};
+
+export function currentTeaReducer(state = initialState, action)
+{
+    switch (action.type)
+    {
+        default:
+            return state;
+    }
+
+
+}
---
+++ src/reducer/diaryEntryReducer.jsx
@@ -0,0 +1,26 @@
+/*
+planned to add (Origin, Vendor, Link)
+{
+teaName:
+steeps:[[{
+ level:
+ note:
+ Detail:
+ }]]
+}
+ */
+
+
+const initialState = {
+
+    diaryEntry: [],
+};
+
+export function diaryEntryReducer(state = initialState, action) {
+    switch (action.type) {
+        default:
+            return state;
+    }
+
+
+}

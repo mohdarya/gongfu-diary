7640080230fd6a7bcd6bd62fee48575a49cb54b9
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-09-05 18:08:36 +0400

    added new reducers to root reducer

--- src/reducer/rootReducer.jsx
+++ src/reducer/rootReducer.jsx
@@ -1,8 +1,11 @@
 import {combineReducers} from 'redux';
+import {diaryEntryReducer} from "./diaryEntryReducer";
+import {currentTeaReducer} from "./currentTeaReducer";
 
 
 const rootReducer = combineReducers({
-
+    Diary: diaryEntryReducer,
+    TeaAvailable: currentTeaReducer
 });
 
 export default rootReducer;

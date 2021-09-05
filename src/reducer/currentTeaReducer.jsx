b1fe91edb8e23c6b9d2b55d63d593b505194c6a3
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-09-05 19:35:43 +0400

    added reducer functions

--- src/reducer/currentTeaReducer.jsx
+++ src/reducer/currentTeaReducer.jsx
@@ -1,6 +1,7 @@
 /*
 
 {
+teaID:
 teaName:
 Origin:
 Vendor:
@@ -13,10 +14,32 @@ const initialState = {
     teaAvailable: [],
 };
 
-export function currentTeaReducer(state = initialState, action)
-{
-    switch (action.type)
-    {
+export function currentTeaReducer(state = initialState, action) {
+    switch (action.type) {
+
+         case 'ADD_TEA':
+            return {
+                ...state,
+                teaAvailable: [...state.teaAvailable, ...action.newEntry]
+
+            }
+        case 'REMOVE_TEA':
+            return {
+                ...state,
+                teaAvailable: state.teaAvailable.filter(item => item.teaID !== action.teaID)
+
+            }
+
+        case 'EDIT_TEA':
+            const index = state.teaAvailable.findIndex(item => item.teaID !== action.teaID)
+            const newArray = [...state.teaAvailable]
+            newArray[index] = [...state.diaryEntry[index], ...action.newEntry]
+
+            return {
+                ...state,
+                teaAvailable: newArray
+
+            }
         default:
             return state;
     }

59cd8160de14ab46bc00ccbe68ebf300dba16241
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-09-05 19:20:26 +0400

    added diary reducer functions

--- src/reducer/diaryEntryReducer.jsx
+++ src/reducer/diaryEntryReducer.jsx
@@ -1,7 +1,9 @@
 /*
 planned to add (Origin, Vendor, Link)
 {
+
 teaName:
+sessionID:
 steeps:[[{
  level:
  note:
@@ -18,6 +20,31 @@ const initialState = {
 
 export function diaryEntryReducer(state = initialState, action) {
     switch (action.type) {
+        case 'ADD_STEEP':
+            const index = state.diaryEntry.findIndex(item => item.sessionID !== action.sessionID)
+            const newArray = [...state.diaryEntry]
+            newArray[index].steeps = [...state.diaryEntry[index].steeps, ...action.newSteep]
+
+            return {
+                ...state,
+                diaryEntry: newArray
+
+            }
+        case 'ADD_ENTRY':
+            return {
+                ...state,
+                diaryEntry: [...state.diaryEntry, ...action.newEntry]
+
+            }
+        case 'REMOVE_ENTRY':
+            return {
+                ...state,
+                diaryEntry: state.diaryEntry.filter(item => item.sessionID !== actions.sessionID)
+
+            }
+
+
+
         default:
             return state;
     }

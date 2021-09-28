657fc0b07e39effc3789481bddb8db71fa781345
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-09-28 20:06:31 +0400

    feat: timer part

--- src/containers/diaryEntry.jsx
+++ src/containers/diaryEntry.jsx
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+import {Image, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
 import {useNavigation, useRoute} from "@react-navigation/core";
 import {addEntry, addSteep} from "../action/diaryEntryAction";
 import {connect} from 'react-redux';
 import RadarChart from "../components/radarChart";
+import {Line} from "react-native-svg";
 
 function DiaryEntry(props) {
 
@@ -15,139 +16,21 @@ function DiaryEntry(props) {
             backgroundColor: 'white',
 
         },
-        topBar: {
-            flexDirection: 'row',
-            justifyContent: 'flex-end',
-            alignItems: 'center',
-            flex: 1,
-            margin: 10,
-            marginTop: 0,
+        topPart: {
+            height: 170,
+            width: '100%',
 
 
         },
-        timerView: {
-            alignItems: 'center',
-            marginBottom: 20,
-            marginLeft: 10,
-            marginRight: 10,
-            flexDirection: 'row',
-            justifyContent: 'space-around',
-            flex: 2,
-        },
-        countdownTimer: {
+        topPartBar: {
+            height: 250,
+            width: '100%',
 
-            backgroundColor: 'grey',
-            borderRadius: 15,
-            height: 160,
-            width: 160,
             alignItems: 'center',
-            justifyContent: 'center',
-        },
-        countdownTimerText: {
-            textAlign: 'center',
-            fontSize: 50,
-        },
-        endButton: {
-            backgroundColor: 'grey',
-            borderRadius: 5,
-            height: '45%',
-            width: '25%',
-            alignItems: 'center',
-            justifyContent: 'center',
-        },
-        endButtonText: {
-            textAlign: 'center',
-            bottom: '5%',
-            fontSize: 23,
-        },
-        incrementView: {
-            backgroundColor: 'grey',
-            borderRadius: 20,
-            flexDirection: 'column',
-            borderTopLeftRadius: 0,
-            borderBottomRightRadius: 0,
-            justifyContent: 'center',
-            alignContent: 'center',
-            height: 90,
-            width: 150,
-        },
-        incrementTag: {
-            fontSize: 17,
-            alignSelf: 'center',
-        }, incrementNumber: {
-            alignSelf: 'center',
-            fontSize: 30,
-            color: 'black',
-        }, teaNameView: {
-            height: 60,
-            flexDirection: 'row',
-            marginBottom: 20,
-            marginTop: 10,
-            marginLeft: 20,
-            marginRight: 20,
-            backgroundColor: 'grey',
-            borderRadius: 20,
-            borderTopLeftRadius: 0,
-            borderBottomRightRadius: 0,
-            justifyContent: 'flex-start',
-        }, teaFlavorView: {
-            backgroundColor: 'grey',
-            flex: 5,
-            marginLeft: 20,
-            marginRight: 20,
-            borderRadius: 20,
-            flexDirection: 'column',
-            borderTopLeftRadius: 0,
-            borderBottomRightRadius: 0,
-        }, doneButtonView: {
-            flex: 2,
-            justifyContent: 'center',
-            alignContent: 'center',
-            alignItems: 'center',
-        },
-        teaNameTag: {
-            fontSize: 17,
-            margin: 5,
-            marginTop: 1,
-        },
-        teaName: {
-            height: 25,
-            width: 'auto',
-            textAlign: 'center',
-            textAlignVertical: 'top',
-            fontSize: 15,
-            fontWeight: 'bold',
-            color: 'black',
+            backgroundColor: '#2A9D8F',
+            borderBottomLeftRadius: 93,
+            borderBottomRightRadius: 93,
 
-        },
-        teaNameTextView: {
-            width: 'auto',
-            right: '10%',
-
-            justifyContent: 'center',
-            alignItems: 'center',
-            alignSelf: 'center',
-        },
-        graphView: {
-            alignItems: 'center',
-            justifyContent: 'center',
-            flex: 1,
-        }, doneButton: {
-            backgroundColor: 'grey',
-            width: 200,
-            height: 50,
-            borderRadius: 10,
-            justifyContent: 'center',
-            alignContent: 'center',
-        },
-        doneButtonText: {
-            textAlign: 'center',
-            bottom: '5%',
-            fontSize: 25,
-        },
-        incrementNumberView: {
-            justifyContent: 'center',
-            height: 60,
         },
 
 
@@ -155,7 +38,8 @@ function DiaryEntry(props) {
 
     const navigation = useNavigation()
     const route = useRoute()
-    const {teaName, startingTime} = route.params
+    let teaName = 'red Tiger'
+    let startingTime = 20
     const [first, setFirst] = useState(true)
     const [currentTime, setCurrenTime] = useState(parseInt(startingTime))
     const [countdownTimer, setCountdownTimer] = useState(parseInt(startingTime))
@@ -239,81 +123,37 @@ function DiaryEntry(props) {
 
             <View style={styles.container}>
 
-                <View style={styles.topBar}>
-
-
-                    <TouchableOpacity style={styles.endButton} activeOpacity={1} onPress={endButtonAction}>
-                        <Text style={styles.endButtonText}>
-                            End
-                        </Text>
-                    </TouchableOpacity>
-
-                </View>
-                <View style={styles.timerView}>
-                    <View style={styles.countdownTimer}>
-                        <Text style={styles.countdownTimerText}>
-                            {countdownTimer}
-                        </Text>
-                    </View>
-                    <View style={styles.incrementView}>
-                        <View>
-                            <Text style={styles.incrementTag}>
-                                Increment
-                            </Text>
-                        </View>
-
-                        <View style={styles.incrementNumberView}>
-                            <TextInput selectTextOnFocus={true} keyboardType={"number-pad"}
-                                       style={styles.incrementNumber}
-                                       onChangeText={(text) => {
-                                           setIncrement(parseInt(text))
-                                       }}>
-                                {increment}
-                            </TextInput>
+                <View style={styles.topPart}>
+                    <View style={styles.topPartBar}>
+                        <View style={{ top: 20,width: 80, height: 80, backgroundColor: '#E9C46A', borderRadius:100, justifyContent: 'center', alignItems: 'center'}}>
+                            <TouchableOpacity activeOpacity={1} style={{width: 70, height: 70}}>
+                                <Image style={{height: '100%', width: '100%'}} source={require('../img/add.png')}/>
+                            </TouchableOpacity>
                         </View>
                     </View>
+                    <View style={{ top: '70%',left: '20%',width: '60%', height: 300, backgroundColor: '#E9C46A', borderRadius:30, position: "absolute", alignContent: 'center', justifyContent: 'flex-start'}}>
+                        <Text style={{alignSelf: 'center', fontSize: 60, color: '#264653', fontWeight: 'bold'}}>
+                            16:40
+                        </Text>
+                        <Text style={{alignSelf: 'center', fontSize: 25, color: '#264653'}}>
+                            Timer
+                        </Text>
+                       <View style={{width: '80%', marginTop: 5, alignSelf: 'center', height: 2, backgroundColor: '#2A9D8F'}}/>
+                        <Text style={{alignSelf: 'center', marginTop: 5,fontSize: 25, color: '#264653', fontWeight: 'bold'}}>
+                            500
+                        </Text>
+                        <Text style={{alignSelf: 'center', fontSize: 25, color: '#264653'}}>
+                            Increment
+                        </Text>
+
+                        <Text style={{alignSelf: 'center',marginTop: 15, fontSize: 15, color: '#264653', fontWeight: 'bold', textAlign: 'center'}}>
+                            Feng Qing Ye Sheng Hong Cha Wild Tree Purple Black Tea
+                        </Text>
+
+
+                    </View>
 
                 </View>
-                <View style={styles.teaNameView}>
-                    <View style={{height: 'auto', width: '15%', }}>
-                        <Text style={styles.teaNameTag}>
-                            Tea
-                        </Text>
-                    </View>
-
-                    <View style={{width: '85%', justifyContent: 'center', height: '100%'}}>
-                    <TouchableOpacity style={styles.teaNameTextView}>
-                        <Text style={styles.teaName}
-                        >
-                            {teaName}
-                        </Text>
-                    </TouchableOpacity>
-                    </View>
-                </View>
-
-                <TouchableOpacity style={styles.teaFlavorView}
-                                  activeOpacity={1}
-onPress={goToFlavorSelection}
-                >
-
-                    <View>
-                        <Text style={styles.teaNameTag}>
-                            Flavor
-                        </Text>
-                    </View>
-                    <View style={styles.graphView}>
-
-                    <RadarChart steeps={{...steepData}}/>
-                    </View>
-                </TouchableOpacity>
-                <View style={styles.doneButtonView}>
-                    <TouchableOpacity style={styles.doneButton} onPress={startInterval} activeOpacity={1}>
-                        <Text style={styles.doneButtonText}>
-                            Start
-                        </Text>
-                    </TouchableOpacity>
-                </View>
-
 
             </View>
 

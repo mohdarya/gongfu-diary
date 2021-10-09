2ead8b635e1b85db2efcab5e955012b5c65a325b
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-10-09 14:13:13 +0400

    feat: select random tea only if more than 2 teas available

--- src/containers/homescreen.jsx
+++ src/containers/homescreen.jsx
@@ -500,12 +500,13 @@ function HomeScreen(props) {
                     </TouchableOpacity>
                     <TouchableOpacity activeOpacity={1} onPress={() => {
                         let active = Object.entries(props.teaAvailable).filter(([key, value]) => value.status === 'active')
-                        let randomTea = active[Math.floor(Math.random() * active.length)]
+                        if(active.length >= 2)
+                        {let randomTea = active[Math.floor(Math.random() * active.length)]
                         navigation.navigate("TeaName", {
                             teaID: randomTea[0],
                             teaName: randomTea[1].teaName
                         })
-
+                            }
                     }} style={{width: 35, height: 32}}>
                         <Image style={{height: '100%', width: '100%'}} source={require('../img/shuffle.png')}/>
                     </TouchableOpacity>

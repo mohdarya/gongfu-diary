e0d0583390d310c3d704840848277420a3b80512
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-10-09 21:41:21 +0400

    fix: needing to press twice to select new tea type

--- src/containers/teaInventoyEdit.jsx
+++ src/containers/teaInventoyEdit.jsx
@@ -331,6 +331,7 @@ function TeaInventoryEdit(props) {
                         {teaData.teaName}
                     </TextInput>
                     <TouchableOpacity  activeOpacity={1} onPress={() => {
+                        Keyboard.dismiss()
                        setTypeModal(true)
                     }} style={{fontSize: 20, height: 45, marginLeft: 3, marginBottom: 15,  borderBottomWidth: 2, borderColor: '#E9C46A'}}>
                         <Text style={{fontSize: 20, color:'white'}}>

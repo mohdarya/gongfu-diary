1d575560b615edc5223c4724a04c1b801f7829a4
mohdarya <mohsen.a.daryanavard@gmail.com>
2021-10-09 11:09:46 +0400

    feat: data reseting confimration modal

--- src/containers/settingsPage.jsx
+++ src/containers/settingsPage.jsx
@@ -24,17 +24,84 @@ function SettingsPage(props) {
             marginRight: 15,
             marginTop: 50,
             fontWeight: 'bold'
-        }
+        }, doneButton: {
+            backgroundColor: '#E9C46A',
+            width: 100,
+            height: 50,
+            borderRadius: 10,
+            justifyContent: 'center',
+            alignContent: 'center',
+        },
+        doneButtonText: {
+            textAlign: 'center',
+            bottom: '5%',
+            fontWeight: 'bold',
+            fontSize: 30,
+            color: '#264653',
+        },
 
 
     });
 
-
+    const [confirmationVisible, setConfirmation] = useState(false)
 
     return (
         <View style={styles.container}>
 
+            <Modal animationType="slide"
+                   transparent={true}
+                   visible={confirmationVisible}
+                   onRequestClose={() => {
 
+                       setConfirmation(!confirmationVisible)
+                   }}>
+                <View style={{height: '50%', width: '100%', justifyContent: 'center',alignSelf: 'center', alignItems: 'center'}}>
+                    <View style={{backgroundColor: 'white' , justifyContent: 'space-around',height: 200, width: '90%',borderRadius:20}}>
+
+                        <Text style={ {
+                            fontSize: 23,
+                            color: 'black',
+                            marginLeft: 20,
+                            marginRight: 20,
+                            textAlign: 'center',
+                            marginTop: 5,
+                            fontWeight: 'bold'
+                        }}>
+                            Are You Sure You Wanna Reset The Data?
+                        </Text>
+
+                        <View style={{flexDirection: 'row',  justifyContent: 'space-around', marginRight: 50, marginLeft: 50,}}>
+                        <TouchableOpacity style={{ backgroundColor: 'grey',
+                            width: 100,
+                            height: 50,
+                            borderRadius: 10,
+                            justifyContent: 'center',
+                            alignContent: 'center',}}  onPress={()=> {
+
+                        }} activeOpacity={1}>
+                            <Text style={ {
+                                textAlign: 'center',
+                                bottom: '5%',
+                                fontWeight: 'bold',
+                                fontSize: 30,
+                                color: 'black',
+                            }}>
+                                Yes
+                            </Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.doneButton}  onPress={()=> {
+                            setConfirmation(!confirmationVisible)
+                        }} activeOpacity={1}>
+                            <Text style={styles.doneButtonText}>
+                                No
+                            </Text>
+                        </TouchableOpacity>
+                        </View>
+                    </View>
+
+                </View>
+
+            </Modal>
 
             <View style={{height: '100%',}}>
 
